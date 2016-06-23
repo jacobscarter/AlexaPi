16e4e7838efc9309f5c0ba350fd766ef807ed260
jacobscarter <jacob@ieksolutions.com>
2016-06-22 23:02:42 -0700

    set up button on GPIO

--- test_button.py
+++ test_button.py
@@ -1,16 +1,14 @@
-import time
 import RPi.GPIO as GPIO
-#initialise a previous input variable to 0 (assume button not pressed last)
-prev_input = 0
+import time
+import os
+
+#adjust for where your switch is connected
+buttonPin = 18
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.IN)
+GPIO.setup(buttonPin,GPIO.IN)
+
 while True:
-  #take a reading
-  input = GPIO.input(18)
-  #if the last reading was low and this one high, print
-  if ((not prev_input) and input):
-    print("Button pressed")
-  #update previous input
-  prev_input = input
-  #slight pause to debounce
-  time.sleep(0.05)
+  #assuming the script to call is long enough we can ignore bouncing
+  if (GPIO.input(buttonPin)):
+    #this is the script that will be called (as root)
+    print("Button Pressed")
